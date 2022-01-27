d8e4bfaa9d94f15cf198e7bb57f31fc3faabe2e2
MadhuAllapureddy <madhuri.a5115@gmail.com>
2022-01-27 12:25:02 +0530

    latest code

---
+++ src/components/assetmanagement/dcumaster/components/NavBar.js
@@ -0,0 +1,28 @@
+import { Typography, AppBar, Box, CssBaseline, Toolbar, IconButton, } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import BasicTabs from '../dcumasterpage/DCUMaster';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+
+const  NavBar=()=>  {
+  return (
+    <>
+    <CssBaseline />
+    <AppBar position="static" style={{ background: '#2E3B55' }}>
+				<Toolbar>
+					<IconButton size="large" edge="start" color="inherit" aria-label="open drawer">
+						<HomeIcon style={{ color: 'black' }} />
+					</IconButton>
+					<IconButton size="large" edge="start" color="inherit" aria-label="open drawer">
+						<ChevronRightIcon style={{ color: 'black' }} />
+					</IconButton>
+					<Typography>Asset Management</Typography>
+				</Toolbar>
+				<Typography style={{ marginRight: 'auto', marginLeft: '20px'}}>Maintain DCU</Typography>
+	</AppBar>
+    <BasicTabs />
+    </>
+  );
+}
+export default NavBar;
---
+++ src/components/assetmanagement/metermaster/NavBar.js
@@ -0,0 +1,26 @@
+import { Typography, AppBar, Box, CssBaseline, Toolbar, IconButton, } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+const NavBar=()=>{// Navbar component
+    return(
+        <div>
+        <CssBaseline />
+        <AppBar position="static" style={{ background: '#2E3B55' }}>
+				<Toolbar>
+					<IconButton size="large" edge="start" color="inherit" aria-label="open drawer">
+						<HomeIcon style={{ color: 'black' }} />
+					</IconButton>
+					<IconButton size="large" edge="start" color="inherit" aria-label="open drawer">
+						<ChevronRightIcon style={{ color: 'black' }} />
+					</IconButton>
+					<Typography>Asset Management</Typography>
+				</Toolbar>
+				<Typography style={{ marginRight: 'auto', marginLeft: '20px' }}>Meter Search</Typography>
+			</AppBar>
+        </div>
+
+    )
+}
+export default NavBar
